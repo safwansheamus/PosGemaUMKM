43a427784d5319bb8a4319d7acc060cc97d04e0e
Safwan Sheamus <cruelpennywise@gmail.com>
2024-07-10 11:06:36 +0700

    creating-hero-components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import banner from "@/images/assetcoba.png";
 
 const Hero = () => {
-  return <div>Hero</div>;
-}
+  return (
+    <div className="w-full max-h-screen relative">
+      <Image
+        src={banner}
+        alt="Banner Image"
+        className="w-full max-h-screen object-contain"
+      />
+      <div className="absolute top-0 w-full h-full bg-black/30 text-gray-100 flex flex-col items-center justify-center">
+        <h2 className="text-7xl lg:text-[150px] font-bold">Kepala Suku</h2>
+        <p className="text-xl md:text-2xl lg:text-5xl font-semibold">
+          Queen of whore, witch
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,37 @@
-import React from 'react'
-import Logo from './Logo';
-import Link from 'next/link';
-import {FiMenu} from 'react-icons/fi'
+import React from "react";
+import Logo from "./Logo";
+import Link from "next/link";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
-    const navigation = [
-        { title: "Home", href: "/" },
-        { title: "Features", href: "/features" },
-        { title: "About me", href: "/about" },
-        { title: "Studio", href: "/studio" },
-      ];
+  const navigation = [
+    { title: "Home", href: "/" },
+    { title: "Features", href: "/features" },
+    { title: "About me", href: "/about" },
+    { title: "Studio", href: "/studio" },
+  ];
   return (
-  <div className='w-full bg-white/70 h-20 shadow-md'>
-    <div className='max-w-screen-xl mx-auto flex items-center justify-between px-4 lg:px-0 h-full'>
+    <div className="w-full bg-white/70 h-20 shadow-md">
+      <div className="max-w-screen-xl mx-auto flex items-center justify-between px-4 lg:px-0 h-full">
         <Logo title="PosBlog" className="text-black" />
-        <div className='hidden md:inline-flex items-center gap-7 text-gray-900 hover:text-black duration-200'>
-          {navigation.map((item)=>(
-              <Link key={item?.title} href={item?.href} className='text-sm uppercase font-semibold relative group overflow-hidden'>
-               {item?.title}
-               <span className='w-full h-[1px] bg-blue-700 absolute inline-block left-0 bottom-0 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-200' /> 
-              </Link>
+        <div className="hidden md:inline-flex items-center gap-7 text-gray-900 hover:text-black duration-200">
+          {navigation.map((item) => (
+            <Link
+              key={item?.title}
+              href={item?.href}
+              className="text-sm uppercase font-semibold relative group overflow-hidden"
+            >
+              {item?.title}
+              <span className="w-full h-[1px] bg-blue-700 absolute inline-block left-0 bottom-0 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-200" />
+            </Link>
           ))}
         </div>
-        <div className='md:hidden'>
-          <FiMenu className='text-2xl'/>
+        <div className="md:hidden">
+          <FiMenu className="text-2xl" />
         </div>
+      </div>
     </div>
-  </div>
   );
 };
 
-
-export default Navbar
+export default Navbar;
